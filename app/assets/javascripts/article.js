8afd5f451aa5bd3c7b6a64b9990361c4f6ea842e
sarony <saron.yitbarek@gmail.com>
2015-08-26 14:56:52 -0400

    WIP

--- app/assets/javascripts/article.js
+++ app/assets/javascripts/article.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-  $("article p.description").hide();
+  $("article .more-info").hide();
   $("a.expand").on("click", function(e){
-    $(this).closest("article").find("p.description").slideToggle("slow");
+    $(this).closest("article").find(".more-info").slideToggle("slow");
+    // $(this).closest("article").find("p.description").slideToggle("slow");
     return false;
   });
 });
