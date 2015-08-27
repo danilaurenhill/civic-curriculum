9ea40b3a87c32b0d33b0dca35d01df5c97757205
sarony <saron.yitbarek@gmail.com>
2015-08-27 14:51:49 -0400

    Styles view link for article

--- app/assets/javascripts/article.js
+++ app/assets/javascripts/article.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
   $("article .more-info").hide();
+  var height = $(".more-info .description").height();
+  $(".more-info .image-box").height(height + 30);
   $("article").on("click", "a.more", function(e){
     $(this).closest("article").find(".more-info").slideDown();
     $(this).closest("article").height("auto");
     $(this).addClass("collapse").removeClass("more")
     $(this).html("<h4>Less</h4>")
+    var height = $(".more-info .description").height();
+    $(".more-info .image-box").height(height + 30);
+    var height = $(".more-info .description").height();
+    $(".more-info .image-box").height(height + 30);
     return false;
   });
   $("article").on("click", "a.collapse", function(e){
@@ -13,9 +19,11 @@ $(document).ready(function(){
     $("article .more-info").animate({display: "none"}, 500);
     $(this).addClass("more").removeClass("collapse");
     $(this).html("<h4>More</h4>")
+    var height = $(".more-info .description").height();
+    $(".more-info .image-box").height(height + 30);
     return false;
   });
-    return false;
+  return false;
 });
 
 
