bb43a4a11bfd5e1985f2a21f4014813bbaa5f55f
danilaurenhill <dani.lauren.hill@gmail.com>
2015-08-27 15:17:03 -0400

    Fixes radio buttons, and WIP

---
+++ app/assets/javascripts/clear.js
@@ -0,0 +1,3 @@
+$('document').ready(function() {
+  $(this).removeClass("clear");
+  $("tag-category sector a", "tag-category problem a", "tag-category method a").removeAttr("href").css("cursor","pointer");
