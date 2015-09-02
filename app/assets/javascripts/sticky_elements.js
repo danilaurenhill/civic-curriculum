e27780f2eefa6c69c4d5f18f39f298805de3cb05
sarony <saron.yitbarek@gmail.com>
2015-09-02 18:29:29 -0400

    Sidebar sticks

--- app/assets/javascripts/sticky_elements.js
+++ app/assets/javascripts/sticky_elements.js
@@ -2,12 +2,19 @@ $(document).ready(function(){
   var $window = $(window),
   $mainQuestions = $('.main-questions'),
   $tagAside = $('aside'),
-  $tagAsideAnchor = $('.aside-anchor'),
+  $tagAsideAnchor = $(".aside-anchor"),
   mainQuestionsTop = $mainQuestions.offset().top;
 
   $window.scroll(function() {
     $mainQuestions.toggleClass('sticky', $window.scrollTop() > mainQuestionsTop);
     $tagAside.toggleClass('sticky', $window.scrollTop() > mainQuestionsTop);
     $tagAsideAnchor.toggleClass('sticky', $window.scrollTop() > mainQuestionsTop);
+    if($window.scrollTop() > mainQuestionsTop){
+      $tagAside.width($(".aside-anchor.sticky").width()-30);
+      $(".main").css("margin-top", "120px");
+    } else {
+      $(".main").css("margin-top", "0px");
+      $tagAside.width($("100%"));
+    };
   });
 });
