11f61797327a97619ed51a8d3ad65d69bd8e6408
sarony <saron.yitbarek@gmail.com>
2015-09-09 00:45:51 -0400

    Fixes expanding articles

--- app/assets/javascripts/article.js
+++ app/assets/javascripts/article.js
@@ -1,8 +1,6 @@
 var leftAlignArticle = function(){
   imageBoxes = $(".article-synopsis > .image-box");
   $.each(imageBoxes, function(i, imageBox){
-    // height = $(this).closest("article").find(".preview").height();
-    // $(this).height(height);
     $(this).closest("article").find(".article-text").css("overflow", "hidden");
   });
 };
@@ -37,47 +35,20 @@ var expandCase = function(){
       $(this).addClass("js-more").removeClass("js-collapse");
     });
   });
-
-  // $("article .more-info").hide();
-
-  // $("article").on("click", ".js-more", function(e){
-  //   var article = $(this).closest("article");
-
-  //   $(article).find(".more-info").slideDown();
-  //   $(article).height("auto");
-
-  //   var height = $(article).find(".description").height();
-  //   $(".more-info .image-box").height(height + 50);
-  //   var moreDivs = $(".js-more");
-  //   $.each(moreDivs, function(m, more){
-  //     $(this).addClass("js-collapse").removeClass("js-more")
-  //   });
-  //   return false;
-  // });
-
-  // $("article").on("click", ".js-collapse", function(e){
-  //   var article = $(this).closest("article");
-  //   $(article).animate({height: 200}, 500);
-  //   $(article).find(".more-info").slideUp();
-
-  //   var collapseDivs = $(".js-collapse");
-  //   $.each(collapseDivs, function(c, collapse){
-  //     $(this).addClass("js-more").removeClass("js-collapse");
-  //   });
-  //   return false;
-  // });
 }
+
+var setImageBoxHeight = function(){
+  var imageBoxes = $(".article-synopsis > .image-box");
+  $.each(imageBoxes, function(i, imageBox){
+    height = $(this).closest("article").find(".article-text").height();
+    $(this).height(height);
+  });
+};
 
 $(document).ready(function(){
   var height = $(".more-info .description").height();
   $(".more-info .image-box").height(height + 30);
-  var imageBoxes = $(".article-synopsis > .image-box");
-  console.log(imageBoxes.length);
-  $.each(imageBoxes, function(i, imageBox){
-    height = $(this).closest("article").find(".article-text").height();
-    $(this).height(height);
-    console.log(height);
-  });
+  setImageBoxHeight();
   expandCase();
   return false;
 });
