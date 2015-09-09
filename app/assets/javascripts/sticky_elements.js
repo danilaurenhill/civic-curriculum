66a97935fd4e21702c21b44d1dbb18cb080fd1c3
sarony <saron.yitbarek@gmail.com>
2015-09-09 10:33:13 -0400

    Sticky aside works, but header doesn't stick

--- app/assets/javascripts/sticky_elements.js
+++ app/assets/javascripts/sticky_elements.js
@@ -1,20 +1,27 @@
 $(document).ready(function(){
-  var $window = $(window),
   $mainQuestions = $('.main-questions'),
   $tagAside = $('aside'),
   $tagAsideAnchor = $(".aside-anchor"),
   mainQuestionsTop = $mainQuestions.offset().top;
+  sidebarTop = $tagAsideAnchor.offset().top;
 
-  $window.scroll(function() {
-    $mainQuestions.toggleClass('sticky', $window.scrollTop() > mainQuestionsTop);
-    $tagAside.toggleClass('sticky', $window.scrollTop() > mainQuestionsTop);
-    $tagAsideAnchor.toggleClass('sticky', $window.scrollTop() > mainQuestionsTop);
-    if($window.scrollTop() > mainQuestionsTop){
+  $(window).scroll(function() {
+    // $mainQuestions.toggleClass('sticky', $(window).scrollTop() >= mainQuestionsTop);
+    // $tagAside.toggleClass('sticky', $(window).scrollTop() >= mainQuestionsTop);
+    // $tagAsideAnchor.toggleClass('sticky', $(window).scrollTop() >= mainQuestionsTop);
+
+    if($(window).scrollTop() >= sidebarTop){
+      // $mainQuestions.addClass("sticky");
+      $tagAsideAnchor.addClass("sticky");
+      $tagAside.addClass("sticky");
       $tagAside.width($(".aside-anchor.sticky").width()-30);
-      $(".main").css("margin-top", "120px");
+      // $(".main").css("margin-top", "120px");
     } else {
-      $(".main").css("margin-top", "0px");
+      // $mainQuestions.removeClass("sticky");
+      $tagAside.removeClass("sticky");
+      $tagAsideAnchor.removeClass("sticky");
       $tagAside.width($("100%"));
+      // $(".main").css("margin-top", "0px");
     };
   });
 });
